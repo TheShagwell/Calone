9fa79bf3109ea9ea029e025105f3315d65e4c910
TheShagwell <kelvinerewharighe@gmail.com>
2023-11-18 23:52:37 +0100

    Added the 'story' section of the app

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header'
 import Podcast from '@/components/Podcast'
+import Story from '@/components/Story'
 import Image from 'next/image'
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <Header/>
       <Podcast/>
+      <Story/>
     </>
   )
 }
---
+++ components/Story.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './Button'
+
+const Story = () => {
+  return (
+    <div className='max-container padding-container mt-34'>
+        <h2 className='bold-72 font-clashDisplay text-offWhite-30 mb-6'>Connect with Your Audience and Amplify Your Message</h2>
+        <p className='w-[646px] text-offWhite-10 mb-6'>We believe everyone's stories can and should be heard, so we're giving creators around the world, from first-time podcasters to pros a powerful platform to share their stories.</p>
+        <Button type="button" title="Get started" icon="/_plus.svg" variant="btn_white"/>
+    </div>
+  )
+}
+
+export default Story
