570e1e706e5a8778e0eedb9ef465343906b824ec
TheShagwell <kelvinerewharighe@gmail.com>
2024-01-01 20:50:07 +0100

    go full updated

---
+++ app/pricing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='max-container padding-container py-8 px-[96px]'>
+        page
+    </div>
+  )
+}
+
+export default page
