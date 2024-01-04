1632728d280fe8e3f9e86bbcb330d956585a0f94
TheShagwell <kelvinerewharighe@gmail.com>
2024-01-04 23:38:50 +0100

    added

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div className='max-container'>
+    <div className='max-container padding-container'>
       <h1>Let's know the features</h1> 
     </div>
   )
